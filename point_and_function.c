6762db08c787667fd252f68e0a8c0e66e3273e8c
Cezorn <96648434+Dey410@users.noreply.github.com>
2026-01-14 07:59:08 +0000

    Review point and function

---
+++ point_and_function.c
@@ -0,0 +1,17 @@
+//设计一个函数，传入两个int参数，并交换这两个参数的值。
+#include <stdio.h>
+
+void swap(int *p1,int *p2){
+    int temp;
+    temp = *p1;*p1 = *p2;*p2 = temp;
+}
+
+int main(){
+    int m = 5;
+    int n = 10;
+    int *p1 = &m,*p2 = &n;
+    printf("m=%d,n=%d\n",*p1,*p2);
+    swap(p1,p2);
+    printf("m=%d,n=%d\n",*p1,*p2);
+    return 0;
+}
